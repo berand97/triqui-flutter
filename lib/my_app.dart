2cb7ba1714bbf622e883878d4a8805c2c3ecbf29
Andres Fabian Patiño Bermudez <97bermudez.andres@gmail.com>
2023-08-25 00:33:46 -0500

    Agregado la logica principal del juego.ademas, los assets de O y X, material y fuente Poppins

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'view/screen/home_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      theme: _buildTheme(Brightness.light),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
+
+  ThemeData _buildTheme(brightness) {
+  var baseTheme = ThemeData(brightness: brightness, useMaterial3: true,);
+
+  return baseTheme.copyWith(
+    textTheme: GoogleFonts.poppinsTextTheme(baseTheme.textTheme),
+  );
+}
 }
